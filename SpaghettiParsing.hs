7afb17d06635f56e9cfb00543ed9336b914abd33
Anthoney B. Arkadie <milordog1@hotmail.com>
2016-12-14 12:50:36 -0700

    Demo Day
    More like demo yay

--- SpaghettiParsing.hs
+++ SpaghettiParsing.hs
@@ -36,6 +36,8 @@ scrubPunct sent = [x | x <- sent,  not $ elem x ",.?:;'\"-+=_!@$%&*)("]
 
 lexPrep :: String -> [String]
 lexPrep sent = words $ map toLower $ scrubPunct sent
+
+demoLex = lexPrep "What an -unruly- sentence, There are (aLl sOrTs of) puntuation $things going @on; and it _REALLY bothers: me!"
 ----------------------------------------------------------------------------------------------------
 
 initSpag :: WState
@@ -158,12 +160,129 @@ parseVN = do
            v <- parseVerb
            n <- parseNoun
            return $ SFragVN v n
-           
+----------------------------------------------------------------------------------------------------
+parseCookWin = do word "cook" ()
+                  word "spaghetti" ()
+                  return "The spaghetti has been cooked!"
+parseCookFail = do word "cook" ()
+                   n <- parseNoun
+                   return "You can't cook that!"
+parseThrow = do word "throw" ()
+                n <- parseNoun
+                word "at" ()
+                n2 <- parseNoun
+                return "Careful!  Those are important."
+parseAttack = do word "attack" ()
+                 parseNoun
+                 return "This isn't the environment for violence!"
+parseTurn = do word "turn" ()
+               n <- parseNoun
+               return "You get the sneaking suspicion that turning that would be fruitless."
+parseOpenWin = do word "open" ()
+                  word "cabinet" ()
+                  return "Inside of the cabinet is a nice jar of spaghetti sauce.  If only there was cooked spaghetti to put it on."
+parseOpenLose = do word "open" ()
+                   parseNoun
+                   return "You realize that there's no way to open that"
+parseGrab1 = do word "grab" ()
+                word "spaghetti" ()
+                return "You handle the brittle strands of the spaghetti with care."
+parseGrab2 = do word "grab" ()
+                word "plate" ()
+                return "You hold the plate and fondly think about how good some nice cooked spaghetti would look on it."
+parseGrab3 = do word "grab" ()
+                word "pot" ()
+                return "You hold the handle firm and think about how much heavier this pot would be if there was water in it."
+parseGrab4 = do word "grab" ()
+                word "pan" ()
+                return "As you caress the pan, you think about how absurd it would be to cook spaghetti in a pan."
+parseGrab5 = do word "grab" ()
+                word "fork" ()
+                return "You hold the fork in between your fingers and practise your spaghetti twirling technique."
+parseGrab6 = do word "grab" ()
+                word "sauce" ()
+                return "You inspect the sauce, it's your favorite brand; goes well with spaghetti."
+parseGrab7 = do word "grab" ()
+                word "cheese" ()
+                return "The cheese is squishy, as cheese should be."
+parseGrab8 = do word "grab" ()
+                word "table" ()
+                return "The table is nice and sturdy.  It won't budge.  The perfect surface on which to enjoy spaghetti."
+parseGrabFail = do word "grab" ()
+                   parseNoun
+                   return "You can't really grab that..."
+parseGrab = parseGrab1 |||
+            parseGrab2 |||
+            parseGrab3 |||
+            parseGrab4 |||
+            parseGrab5 |||
+            parseGrab6 |||
+            parseGrab7 |||
+            parseGrab8 |||
+            parseGrabFail
+parsePut1 = do word "put" ()
+               word "water" ()
+               word "in" ()
+               word "pot" ()
+               return "The water looks lonely in the pot.  It yearns for the spaghetti."
+parsePut2 = do word "put" ()
+               word "spaghetti" ()
+               word "in" ()
+               word "pot" ()
+               return "Now that looks like some spaghetti that's ready to be cooked!"
+parsePut3 = do word "put" ()
+               word "sauce" ()
+               word "on" ()
+               word "spaghetti" ()
+               return "That's some saucy spaghetti!"
+parsePut4 = do word "put" ()
+               word "plate" ()
+               word "in" ()
+               word "sink" ()
+               return "Time to clean the dishes already?"
+parsePut5 = do word "put" ()
+               word "fork" ()
+               word "in" ()
+               word "spaghetti" ()
+               return "Stick a fork in it, because it's done!"
+parsePutFail = do word "put" ()
+                  o <- parseNoun
+                  p <- parsePrep
+                  s <- parseNoun
+                  return "You don't think that goes there..."
+parsePut = parsePut1 |||
+           parsePut2 |||
+           parsePut3 |||
+           parsePut4 |||
+           parsePut5 |||
+           parsePutFail
+
+
+
+
+
+
+
+
+parseStringRes = parseCookWin |||
+                 parseCookFail |||
+                 parseThrow |||
+                 parseAttack |||
+                 parseTurn |||
+                 parseOpenWin |||
+                 parseOpenLose |||
+                 parseGrab |||
+                 parsePut
+-- Put
+-- Spaghetti | Plate | Pot | Pan | Stove | Fork | Water | Sink | Cabinet | Sauce | Cheese | Table
 test :: Parser a -> String -> [a]
 test p s = map fst $ runParser p toks
            where toks = lexPrep s
+           
+demoParse1 = test parsePut "grab cheese"
+demoParse2 = test parseGrab "grab cheese"
 ----------------------------------------------------------------------------------------------------
-main = do
+{-main = do
        putStrLn $ description initSpag
        act <- getLine
        let ans = head $ test parseVN act
@@ -178,3 +297,16 @@ main = do
                putStrLn "Press enter to try again"
                ans <- getLine
                main
+               
+               -}
+main = do
+       putStrLn "\nDemo day yay!\n"
+       act <- getLine
+       let ans = test parseStringRes act
+       if ans == [] then do
+                         putStrLn "Sorry, I don't understand."
+                         main
+                    else do
+                         let nans = head ans
+                         putStrLn nans
+                         main
